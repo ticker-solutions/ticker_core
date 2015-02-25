c755490fa24f64394ddce74b4ccbe85a13e584af
Garry Newball <gnewball@ticker.solutions>
2015-02-24 19:46:43 -0500

    Entity Filtering feature

--- src/main/java/solutions/ticker/core/TickerApp.java
+++ src/main/java/solutions/ticker/core/TickerApp.java
@@ -1,7 +1,10 @@
 package solutions.ticker.core;
 
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.message.GZipEncoder;
+import org.glassfish.jersey.message.filtering.EntityFilteringFeature;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.filter.EncodingFilter;
 
 import solutions.ticker.core.tools.CORSResponseFilter;
 
@@ -11,5 +14,7 @@ public class TickerApp extends ResourceConfig  {
 		packages("solutions.ticker.core");
 		register(CORSResponseFilter.class);
 		register(MultiPartFeature.class);
+		register(EntityFilteringFeature.class);
+		EncodingFilter.enableFor(this, GZipEncoder.class);
 	}
 }
